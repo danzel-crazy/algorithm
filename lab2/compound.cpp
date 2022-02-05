9446c7eb0720ca6f6f7e8c870a0f19f3a1f931a3
danzel-crazy <rickydanzel@gmail.com>
2022-02-05 13:45:09 +0800

    code

--- lab2/compound.cpp
+++ lab2/compound.cpp
@@ -5,31 +5,11 @@
 using namespace std;
 
 int main(){
+    bool in;
     char n;
     string com;
-    map <string, int> sin;
+    map <char, int> sin;
     map <string, int> dou;
-    queue <string> temp;
-
-        if((n - 'A') >= 0 && (n - 'A') <= 26){
-            com.push_back(n);
-        }
-        else if((n - 'a') >= 0 && (n - 'a') <= 26){
-            com.push_back(n);
-            temp.push(com);
-            com.erase();
-        }
-        else if((n - '0') >= 0 && (n - '0') <= 9){
-            if(temp.size() == 1){
-                sin[temp.front()] = n - '0';
-            }
-            else{
-                dou[com] = n - '0';
-            }
-            com.erase();
-        }
-        else if((n - '{') == 0){
-
-
-        } 
+    
+    return 0;
 }
