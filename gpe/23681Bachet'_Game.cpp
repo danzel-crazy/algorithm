20d355860c5194bbad2fb822784e41b77fd0f3a4
danzel-crazy <rickydanzel@gmail.com>
2022-03-13 01:25:37 +0800

    code

---
+++ gpe/23681Bachet'_Game.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
